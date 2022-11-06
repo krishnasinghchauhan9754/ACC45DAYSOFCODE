cc892190e0f46c0e3a194dfe609e64cda4f62adc
krishnasinghchauhan9754 <114243620+krishnasinghchauhan9754@users.noreply.github.com>
2022-11-06 23:53:40 +0530

    Add files via upload

---
+++ numberisdisibleny7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter a number");
+    scanf("%d",&a);
+    if(a%7==0)
+    {
+        printf("a is divisible by 7");
+    }
+    else
+    {
+        printf("a is not divisible by 7");
+    }
+}
